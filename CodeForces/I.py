ceea89d10eae8fd492e025230bf75abced0d7b0d
Nikolay Andreev <bakasaru@list.ru>
2020-03-05 23:20:05 +0300

    Task I for 1000 points

--- CodeForces/I.py
+++ CodeForces/I.py
@@ -13,6 +13,6 @@ y_mean = sum(Y) / len(Y)
 
 numerator = sum([(x - x_mean) * (y - y_mean) for x, y in zip(X, Y)])
 denominator = (sum([(x - x_mean) ** 2 for x in X]) * sum([(y - y_mean) ** 2 for y in Y])) ** 0.5
-ro = numerator / denominator
+ro = numerator / denominator if denominator != 0 else 0
 
 print(f'{ro:.9f}')

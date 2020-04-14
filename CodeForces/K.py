c2dc4017d45b66b157436f3c2063118ef70d5c53
Nikolay Andreev <bakasaru@list.ru>
2020-04-14 23:41:53 +0300

    Task K for 1000 points
    It seems that formula for sum of pairwise differences of elements in
    list can be modified as sum of elements with some coefficients

--- CodeForces/K.py
+++ CodeForces/K.py
@@ -1,9 +1,11 @@
 def sum_differences(nums: list):
+    l = len(nums)
+    coef = range(-(l - 1), l, 2)
     s = 0
-    for a in nums:
-        for b in nums:
-            s += abs(a - b)
-    return s
+    for i in range(l):
+        s += coef[i] * nums[i]
+
+    return s * 2
 
 
 k = int(input())
@@ -15,6 +17,10 @@ for i in range(n):
     x, y = map(int, input().split())
     nums_in_cat[y].append(x)
     all_nums.append(x)
+
+all_nums.sort()
+for i in range(1, k + 1):
+    nums_in_cat[i].sort()
 
 whole = sum_differences(all_nums)
 inner = 0

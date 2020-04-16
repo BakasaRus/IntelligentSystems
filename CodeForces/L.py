e2f1078213764741f8b6b0c0ac234efe4374cfc8
Nikolay Andreev <bakasaru@list.ru>
2020-04-16 19:51:44 +0300

    Task L for 961 points
    Some TLEs, strange

---
+++ CodeForces/L.py
@@ -0,0 +1,25 @@
+k = int(input())
+n = int(input())
+count = {x: {} for x in range(1, k + 1)}
+count_x = {x: 0 for x in range(1, k + 1)}
+x_y = {x: set() for x in range(1, k + 1)}
+
+for i in range(n):
+    x, y = map(int, input().split())
+    count_x[x] += 1
+    if y in x_y[x]:
+        count[x][y] += 1
+    else:
+        count[x][y] = 1
+        x_y[x].add(y)
+
+dy = 0
+for x in range(1, k + 1):
+    ey = 0
+    ey2 = 0
+    for y in x_y[x]:
+        ey += (count[x][y] / count_x[x]) * y
+        ey2 += (count[x][y] / count_x[x]) * y * y
+    dy += (ey2 - ey ** 2) * (count_x[x] / n)
+
+print(dy)

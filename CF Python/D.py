7faea410c55ee461a6065c0a7ab205accaa9d52b
Nikolay Andreev <bakasaru@list.ru>
2020-04-21 23:28:25 +0300

    Task D for 998 points

--- CF Python/D.py
+++ CF Python/D.py
@@ -1,13 +1,12 @@
 from typing import List
 from random import randint
-from decimal import Decimal
-from copy import deepcopy
+from math import sqrt
 
-Vector = List[Decimal]
+Vector = List[float]
 Matrix = List[Vector]
 
 
-def multiply_scalar(x: Vector, y: Vector) -> Decimal:
+def multiply_scalar(x: Vector, y: Vector) -> float:
     return sum([a * b for a, b in zip(x, y)])
 
 
@@ -16,7 +15,7 @@ def multiply_matrices(x: Matrix, y: Matrix) -> Matrix:
     cx = len(x[0])
     ry = len(y)
     cy = len(y[0])
-    res = [[Decimal(0)] * cy for i in range(rx)]
+    res = [[float(0)] * cy for i in range(rx)]
     for i in range(rx):
         for j in range(cy):
             for ii in range(cx):
@@ -26,36 +25,41 @@ def multiply_matrices(x: Matrix, y: Matrix) -> Matrix:
 
 
 def invert(x: Matrix) -> Matrix:
-    source = deepcopy(x)
-    indices = list(range(len(source)))
-    l = len(source)
+    indices = list(range(len(x)))
+    l = len(x)
     for index in indices:
         for j in range(l, l * 2):
-            source[index].append(Decimal(1) if (index + l == j) else Decimal(0))
+            x[index].append(float(1) if (index + l == j) else float(0))
 
     for index in indices:
-        t = source[index][index]
+        if x[index][index] == 0:
+            for j in range(index + 1, l):
+                if x[j][index] != 0:
+                    for k in range(2 * l):
+                        x[j][k], x[index][k] = x[index][k], x[j][k]
+                    break
+        t = x[index][index]
         for j in range(index, 2 * l):
-            source[index][j] /= t
+            x[index][j] /= t
         for j in range(index + 1, l):
-            t2 = source[j][index]
+            t2 = x[j][index]
             for k in range(index, l * 2):
-                source[j][k] -= source[index][k] * t2
+                x[j][k] -= x[index][k] * t2
 
     for i in reversed(indices):
         for j in range(i - 1, -1, -1):
-            t = source[j][i]
+            t = x[j][i]
             for k in range(l, l * 2):
-                source[j][k] -= source[i][k] * t
+                x[j][k] -= x[i][k] * t
 
-    res = [row[-1:-(l + 1):-1][::-1] for row in source]
+    res = [row[-1:-(l + 1):-1][::-1] for row in x]
     return res
 
 
 def sgd(features: Matrix, res: Vector, n: int, m: int) -> Vector:
-    n_iter = 6666
-    lr = Decimal(9e-4)
-    w = [Decimal(0.)] * (m + 1)
+    n_iter = 10000
+    lr = float(5e-4)
+    w = [float(0.)] * (m + 1)
     for i in range(n_iter):
         idx = randint(0, n - 1)
         x = features[idx]
@@ -85,19 +89,19 @@ def main():
     features, res = [], []
 
     for i in range(n):
-        data = [Decimal(x) for x in input().split()]
+        data = [float(x) for x in input().split()]
         features.append(data[:-1])
-        features[-1].append(Decimal(1))
+        features[-1].append(float(1))
         res.append(data[-1])
 
     answer = []
 
-    if n * m <= 10000:
+    if n * m <= 100:
         answer = linear_regression(features, res)
     else:
         ey = sum(res) / len(res)
         ey2 = sum([x ** 2 for x in res]) / len(res)
-        dy = (ey2 - ey ** 2).sqrt()
+        dy = sqrt(ey2 - ey ** 2)
 
         dx, ex = [], []
 
@@ -105,15 +109,15 @@ def main():
         for i in range(m):
             ex.append(sum([row[i] for row in features]) / n)
             ex2 = sum([row[i] ** 2 for row in features]) / n
-            sq = (ex2 - ex[-1] ** 2).sqrt()
-            dx.append(sq if sq >= Decimal(1e-6) else Decimal(1))
+            sq = sqrt(ex2 - ex[-1] ** 2)
+            dx.append(sq if sq >= float(1e-6) else float(1))
             for j in range(n):
                 features[j][i] = (features[j][i] - ex[-1]) / dx[-1]
-            dx.append(Decimal(1))
-            ans = sgd(features, y2, n, m)
-            ans = [ans[idx] * dy / dx[idx] for idx in range(m + 1)]
-            last = ans[-1]
-            answer = ans[:-1] + [(last - sum([ans[i] * ex[i] for i in range(m)]) + ey)]
+
+        dx.append(float(1))
+        answer = sgd(features, y2, n, m)
+        answer = [answer[idx] * dy / dx[idx] for idx in range(m + 1)]
+        answer[-1] = answer[-1] - sum([answer[i] * ex[i] for i in range(m)]) + ey
 
     print(*answer, sep='\n')
 
